872899d0cafb6138a26671d96f23a11eaae0e1a7
Kathy * <Kathy@KathyLambert.me>
2019-05-07 22:34:47 -0400

    Added inital components to the App.js component and created coresponding component.js files with stater code.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import SearchBar from './components/SearchBar'
+import NavBar from './components/NavBar'
+import RoomList from './components/RoomList'
+import SendMessageForm from './components/SendMessageForm'
+import NewRoomForm from './components/NewRoomForm'
+import MessageList from './components/MessageList'
 
 class App extends React.Component {
   render() {
@@ -9,7 +15,12 @@ class App extends React.Component {
         <header className="App-header">
           <h1>Welcome to CodeMeKathy</h1>
         </header>
-        {/* < /> */}
+        <SearchBar />
+        <NavBar />
+        <RoomList />
+        <MessageList />
+        <SendMessageForm />
+        <NewRoomForm />
       </div>
     );
   }
---
+++ src/components/MessageList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import '../App.css';
+
+export default class MessageList extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
