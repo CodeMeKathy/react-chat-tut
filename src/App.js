75ec69d602e61eeb66f32bc5a6e7da17203e41d3
Kathy * <Kathy@KathyLambert.me>
2019-05-07 21:41:55 -0400

    Modified the App.js component to be class vs function based component.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to CodeMeKathy</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to CodeMeKathy</h1>
+        </header>
+        {/* < /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
