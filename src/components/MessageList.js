861206b930670ccc80e807e4fc35ef71db019464
Kathy * <Kathy@KathyLambert.me>
2019-05-08 10:40:02 -0400

    Added dummyData to MessageList.js component.  Mapped through the dummy data object to return the message text with username.

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
 import '../App.css';
 
+// Dummy Data
+let dummyData = [
+  {
+    senderID: 'janedoe',
+    text: 'Running late.... Be there soon.'
+  },
+  {
+    senderID: 'rainforest2020',
+    text: 'How was your day?'
+  },
+  {
+    senderID: 'coderunner',
+    text: 'Coding fire... Hit you back later.'
+  }
+]
 export default class MessageList extends React.Component {
   render() {
     return (
       <div>
-
+          {dummyData.map((message, index) => {
+             return (
+               <div>
+                <div>{message.senderID}</div>
+                <div>{message.text}</div>
+              </div>
+             )
+          })}
       </div>
     )
   }
